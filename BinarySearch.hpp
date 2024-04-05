448fd1a78e52b7907d02cae6d0cabebc76dc76aa
Konrad Markowski <konradmarkowski@Konrads-MacBook-Air.local>
2024-04-05 17:57:30 +0200

    binary search added

---
+++ BinarySearch.hpp
@@ -0,0 +1,7 @@
+#include "Utils.hpp"
+
+class BinarySearch
+{
+public:
+    static int search(std::vector<int>& vec, int target);
+};
--- main.cpp
+++ main.cpp
@@ -3,15 +3,16 @@
 #include "MergeSort.hpp"
 #include "SelectionSort.hpp"
 #include "BubbleSort.hpp"
+#include "BinarySearch.hpp"
 
 int main()
 {
-	std::vector<int> vec = {5, 3, 8, 4, 6};
-	// InsertionSort::sort(vec);
+	std::vector<int> vec = {1, 4, 5, 7, 8, 10, 12};
 	// std::cout << LinearSearch::search(vec, 10) << std::endl;
+    std::cout << BinarySearch::search(vec, 10) << std::endl;
+    // InsertionSort::sort(vec);
 	// SelectionSort::sort(vec);
 	// MergeSort::sort(vec);
-    BubbleSort::sort(vec);
-	// Utils::print(vec);
+    // BubbleSort::sort(vec);
 	return 0;
 }
